a6bc4794c5c5277269cf6a9a1ab668f8920cb6de
Olivier Ducarme <dume69@gmail.com>
2022-01-10 12:44:21 +0100

    Initial app after section 2

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import router from './routes';
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+import Index from './Index'
+
+window.Vue = require('vue').default;
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components: {
+        'index': Index
+    }
+});
