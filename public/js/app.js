62a3a4092fc7512752af096e8dbf656d3dac4701
Olivier Ducarme <dume69@gmail.com>
2022-01-14 15:43:45 +0100

    Section 3 - Dynamic distribute cards into rows

--- public/js/app.js
+++ public/js/app.js
@@ -5310,6 +5310,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   components: {
@@ -5319,12 +5321,20 @@ __webpack_require__.r(__webpack_exports__);
     return {
       bookables: null,
       loading: false,
-      colNumber: 3
+      colNumber: 4
     };
   },
   computed: {
     rows: function rows() {
       return this.bookables === null ? 0 : Math.ceil(this.bookables.length / this.colNumber);
+    }
+  },
+  methods: {
+    bookablesInRow: function bookablesInRow(row) {
+      return this.bookables.slice((row - 1) * this.colNumber, row * this.colNumber);
+    },
+    fillersInRow: function fillersInRow(row) {
+      return this.colNumber - this.bookablesInRow(row).length;
     }
   },
   created: function created() {
@@ -28339,23 +28349,43 @@ var render = function () {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c("span", [_vm._v("Rows: " + _vm._s(_vm.rows))]),
-    _vm._v(" "),
     _vm.loading
       ? _c("div", [_vm._v("Data is loading ...")])
       : _c(
           "div",
-          _vm._l(_vm.bookables, function (bookable, index) {
-            return _c("bookable-list-item", {
-              key: index,
-              attrs: {
-                title: bookable.title,
-                content: bookable.content,
-                price: bookable.price,
-              },
-            })
+          _vm._l(_vm.rows, function (row) {
+            return _c(
+              "div",
+              { key: "row" + row, staticClass: "row mb-4" },
+              [
+                _vm._l(_vm.bookablesInRow(row), function (bookable, index) {
+                  return _c(
+                    "div",
+                    { key: "col" + row + index, staticClass: "col" },
+                    [
+                      _c("bookable-list-item", {
+                        attrs: {
+                          title: bookable.title,
+                          content: bookable.content,
+                          price: bookable.price,
+                        },
+                      }),
+                    ],
+                    1
+                  )
+                }),
+                _vm._v(" "),
+                _vm._l(_vm.fillersInRow(row), function (f) {
+                  return _c("div", {
+                    key: "filler" + row + f,
+                    staticClass: "col",
+                  })
+                }),
+              ],
+              2
+            )
           }),
-          1
+          0
         ),
   ])
 }
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -2,13 +2,16 @@
   <div>
     <div v-if="loading">Data is loading ...</div>
     <div v-else>
-      <bookable-list-item
-        v-for="(bookable, index) in bookables"
-        :key="index"
-        :title="bookable.title"
-        :content="bookable.content"
-        :price="bookable.price"
-      ></bookable-list-item>
+      <div class="row mb-4" v-for="row in rows" :key="'row' + row">
+        <div class="col" v-for="(bookable, index) in bookablesInRow(row)" :key="'col' + row + index" >
+          <bookable-list-item
+            :title="bookable.title"
+            :content="bookable.content"
+            :price="bookable.price"
+          ></bookable-list-item>
+        </div>
+        <div class="col" v-for="f in fillersInRow(row)" :key="'filler' + row + f"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,14 +26,25 @@ export default {
     return {
       bookables: null,
       loading: false,
-      colNumber: 3,
+      colNumber: 4,
     };
   },
 
   computed: {
     rows() {
-      return this.bookables === null ? 0 : Math.ceil(this.bookables.length / this.colNumber);
+      return this.bookables === null
+        ? 0
+        : Math.ceil(this.bookables.length / this.colNumber);
     },
+  },
+
+  methods: {
+    bookablesInRow(row) {
+      return this.bookables.slice( (row - 1) * this.colNumber, row * this.colNumber );
+    },
+    fillersInRow(row) {
+        return this.colNumber - this.bookablesInRow(row).length;
+    }
   },
 
   created() {
