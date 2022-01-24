51ebc1879ddcb19426c6f93767e47e664b3b71a8
Olivier Ducarme <olivier.ducarme@beplusports.com>
2022-01-24 18:20:11 +0100

    Section 7 - Reviews endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Http\Controllers\Api\BookableController;
+use App\Http\Controllers\Api\BookableReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,5 @@ Route::apiResource('bookables', BookableController::class)
     ->only('index', 'show');
 Route::get('bookables/{id}/availability', BookableAvailabilityController::class)
     ->name('bookables.availability.show');
+Route::get('bookables/{id}/reviews', BookableReviewController::class)
+    -> name('bookables.reviews.index');
