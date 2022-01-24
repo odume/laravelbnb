1f7ede4fd0dffabbb92fb0170083fc2fa9d56e57
Olivier Ducarme <olivier.ducarme@beplusports.com>
2022-01-24 18:19:54 +0100

    Section 7 - Review model, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             BookablesTableSeeder::class,
             BookingTableSeeder::class,
+            ReviewTableSeeder::class,
         ]);
     }
 }
